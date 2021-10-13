dcb34c7c3d5a90f6ddddad8dd6a3726df9401be7
Yuri Jivago <yurijivagoads@gmail.com>
2021-10-13 12:10:54 -0300

    Exercícios do 37 ao 43

---
+++ src/br/com/yurijivago/secao03/Exercicio37.java
@@ -0,0 +1,27 @@
+package br.com.yurijivago.secao03;
+
+import java.util.Scanner;
+
+public class Exercicio37 {
+    public static void main (String [] args){
+        //37. Faça um programa queleia o valor de um produto e imprima o valor com desconto, tendo em vista que o desconto foi de 12%
+
+        Scanner teclado = new Scanner(System.in);
+
+        double valorProduto;
+        double desconto;
+        double valorComDesconto;
+
+        System.out.print("Digite o valor do produto: ");
+        valorProduto = Double.parseDouble(teclado.nextLine());
+
+        desconto = valorProduto * 0.12;
+        valorComDesconto = valorProduto - desconto;
+
+        System.out.printf("Valor original do produto: R$%.2f\n" +
+                "Desconto de 12 %% = %.2f\n" +
+                "Valor com desconto: R$%.2f.", valorProduto, desconto, valorComDesconto);
+
+
+    }
+}
